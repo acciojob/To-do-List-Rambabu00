abf6f06868f995f73c3fba210406c8989e474673
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-03 14:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ var  inputValue=document.querySelector("input").value;
 
 var textnode = document.createTextNode(inputValue);
 node.appendChild(textnode);
+	if(inputValue!=''){
 document.querySelector("ol").appendChild(node);
+document.querySelector("input").value="";
+
+	}
 	
 }
 const btn = document.querySelector("button");
