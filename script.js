b72fd67d8138e2e7b0db015fde467941417fe6a2
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-03 14:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 function todo(){
 	var node = document.createElement("li");
-var  inputValue=document.getElemntById("newTodoInput").value;
-document.write("work");
+var  inputValue=document.querySelector("input").value;
+
 var textnode = document.createTextNode(inputValue);
 node.appendChild(textnode);
-document.getElementById("todoList").appendChild(node);
+document.querySelector("ol").appendChild(node);
 	
 }
 const btn = document.querySelector("button");
